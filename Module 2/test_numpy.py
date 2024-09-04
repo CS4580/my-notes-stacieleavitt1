e2f0f5a102402a9feb7398ded8e0cdce7923e52d
stacieleavitt1 <143892329+stacieleavitt1@users.noreply.github.com>
2024-09-04 10:13:40 -0600

    notes day 3

---
+++ Module 2/test_numpy.py
@@ -0,0 +1,14 @@
+"""First look at Numpy
+"""
+import numpy as np
+
+def main():
+    """Driven Function
+    """
+    # Generate a 1D array with values from 0 to 2
+    arr_1d = np.array([0, 1, 2])
+    print(arr_1d, type(arr_1d))
+
+if __name__ == '__main__':
+    main()
+    
